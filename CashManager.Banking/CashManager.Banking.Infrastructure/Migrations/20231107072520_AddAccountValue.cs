470a4246523c3a7c8fc53389bf93c91c79ec7467
Hugo <hugo.decuq@epitech.eu>
2023-11-07 08:26:38 +0100

    Migration

---
+++ CashManager.Banking/CashManager.Banking.Infrastructure/Migrations/20231107072520_AddAccountValue.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashManager.Banking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Value",
+                table: "Accounts",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Accounts");
+        }
+    }
+}
