8212d4c29bdb2d89c2d9dd43a16a89ceeb0ffa0f
Hugo <hugo.decuq@epitech.eu>
2023-11-08 09:39:02 +0100

    Encryption service

---
+++ CashManager.Banking/CashManager.Banking.Domain/Encryption/IEncryptionService.cs
@@ -0,0 +1,14 @@
+﻿namespace CashManager.Banking.Domain.Encryption;
+
+public interface IEncryptionService
+{
+    public byte[] Encrypt(string data);
+
+    public string Decrypt(byte[] data);
+
+    public string Hash(string data);
+
+    public string HashWithSalt(string data, string salt);
+
+    public string GenereateSalt();
+}
---
+++ CashManager.Banking/CashManager.Banking.Infrastructure/Encryption/EncryptionService.cs
@@ -0,0 +1,31 @@
+﻿using CashManager.Banking.Domain.Encryption;
+
+namespace CashManager.Banking.Infrastructure.Encryption;
+
+internal class EncryptionService : IEncryptionService
+{
+    public byte[] Encrypt(string data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Decrypt(byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Hash(string data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string HashWithSalt(string data, string salt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GenereateSalt()
+    {
+        throw new NotImplementedException();
+    }
+}
