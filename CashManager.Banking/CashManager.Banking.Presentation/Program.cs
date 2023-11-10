1a49504be1d65daf6bb09b907d70695aedd9340b
Hugo <hugo.decuq.34@icloud.com>
2023-11-10 10:04:09 +0100

    Configuration

--- CashManager.Banking/CashManager.Banking.Presentation/Program.cs
+++ CashManager.Banking/CashManager.Banking.Presentation/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 using System.Text;
+using CashManager.Banking.Presentation.Configuration.Schemes;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,9 +45,17 @@ builder.Services
             RequireExpirationTime = true,
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("M+ND73ZDpkzjè_GzdP%354DZok98e4z5d7f75f_çuzd")), //TODO: Replace this hard coded token key
-            ValidIssuer = "CashManager.Banking", //TODO: set this in config file
-            ValidAudience = "CashManager" //TODO: set this in config file
+            ValidIssuer = "CashManager.Banking", //TODO: Set this in config file
+            ValidAudience = "CashManager" //TODO: Set this in config file
         };
+    });
+
+builder.Services
+    .AddAuthentication("ApiKeyScheme")
+    .AddScheme<ApiKeyOption, ApiKeyHandler>("ApiKeyScheme", options =>
+    {
+        options.HeaderField = "ApiKey";
+        options.HeaderAttemptedValue = ",.PjqV#.|X>kgp{?JsygExquC;tVuf5%"; //TODO: Replace this hard coded api key
     });
 
 builder.Services
