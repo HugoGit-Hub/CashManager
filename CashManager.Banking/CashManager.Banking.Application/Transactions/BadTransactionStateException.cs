b289ff66bc59fa734e7463eb46403526966ce5a1
Hugo <hugo.decuq.34@icloud.com>
2023-11-11 15:21:49 +0100

    Update TransactionController

---
+++ CashManager.Banking/CashManager.Banking.Application/Transactions/BadTransactionStateException.cs
@@ -0,0 +1,18 @@
+﻿namespace CashManager.Banking.Application.Transactions;
+
+public class BadTransactionStateException : Exception
+{
+    public BadTransactionStateException()
+    {
+    }
+
+    public BadTransactionStateException(string message)
+        : base(message)
+    {
+    }
+
+    public BadTransactionStateException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
