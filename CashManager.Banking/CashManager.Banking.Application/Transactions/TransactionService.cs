dd039310a11caab63c8d6ff21bc2920ffb34201d
Hugo <hugo.decuq.34@icloud.com>
2023-11-11 15:18:39 +0100

    Update TransactionService

--- CashManager.Banking/CashManager.Banking.Application/Transactions/TransactionService.cs
+++ CashManager.Banking/CashManager.Banking.Application/Transactions/TransactionService.cs
@@ -1,18 +1,31 @@
 ﻿using CashManager.Banking.Domain.Transactions;
+using CashManager.Banking.Infrastructure.Encryption;
 
 namespace CashManager.Banking.Application.Transactions;
 
 internal class TransactionService : ITransactionService
 {
     private readonly ITransactionRepository _transactionRepository;
+    private readonly IEncryptionService _encryptionService;
 
-    public TransactionService(ITransactionRepository transactionRepository)
+    public TransactionService(
+        ITransactionRepository transactionRepository,
+        IEncryptionService encryptionService)
     {
         _transactionRepository = transactionRepository;
+        _encryptionService = encryptionService;
     }
 
-    public async Task<Transaction> Post(Transaction transaction, CancellationToken cancellationToken)
+    public async Task<Transaction> SignAndPost(Transaction transaction, CancellationToken cancellationToken)
     {
+        if (transaction.State != TransactionStateEnum.Pending)
+        {
+            throw new BadTransactionStateException($"Bad transaction state : {transaction.State}");
+        }
+
+        var transactionSignature = _encryptionService.HashWithSalt(transaction);
+        transaction.Signature = transactionSignature;
+
         return await _transactionRepository.Post(transaction, cancellationToken);
     }
 }
