731224bf0c5527f0fd1238fca9a5ccd1575bb80d
Hugo <hugo.decuq.34@icloud.com>
2023-11-11 15:14:16 +0100

    Migration

---
+++ CashManager.Banking/CashManager.Banking.Infrastructure/Migrations/20231110224039_TransactionProperties.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashManager.Banking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransactionProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Signature",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Signature",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Transactions");
+        }
+    }
+}
