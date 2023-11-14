80fd9ec069ba69d68c5cad3880e8b7cf0655d08c
Inès Garcia <ines.garcia@epitech.eu>
2023-11-14 17:36:35 +0100

    Exception catching account not found

--- CashManager.Banking/CashManager.Banking.Application/Transactions/TransactionService.cs
+++ CashManager.Banking/CashManager.Banking.Application/Transactions/TransactionService.cs
@@ -3,6 +3,7 @@ using CashManager.Banking.Domain.Encryption;
 using CashManager.Banking.Domain.Transactions;
 using CashManager.Banking.Domain.User;
 using System.Security.Claims;
+using CashManager.Banking.Domain.Accounts;
 
 namespace CashManager.Banking.Application.Transactions;
 
@@ -10,19 +11,22 @@ internal class TransactionService : ITransactionService
 {
     private readonly ITransactionRepository _transactionRepository;
     private readonly IUsersRepository _usersRepository;
+    private readonly IAccountRepository _accountRepository;
     private readonly IEncryptionService _encryptionService;
     private readonly ICurrentUserService _currentUserService;
 
     public TransactionService(
         ITransactionRepository transactionRepository,
         IUsersRepository usersRepository,
+        IAccountRepository accountRepository,
         IEncryptionService encryptionService,
         ICurrentUserService currentUserService)
     {
         _transactionRepository = transactionRepository;
+        _usersRepository = usersRepository;
+        _accountRepository = accountRepository;
         _encryptionService = encryptionService;
         _currentUserService = currentUserService;
-        _usersRepository = usersRepository;
     }
 
     public async Task<Transaction> SignAndPost(Transaction transaction, CancellationToken cancellationToken)
@@ -31,9 +35,13 @@ internal class TransactionService : ITransactionService
         {
             throw new BadTransactionStateException($"Bad transaction state : {transaction.State}");
         }
-
-        // TODO: Exception with no user accounts
+        var account = await _accountRepository.Get(transaction.Creditor, cancellationToken);
+        if (account == null)
+        {
+            throw new UserAccountNotFoundException($"No account found for this user");
+        }
         var user = await _usersRepository.GetByAccountNumber(transaction.Creditor, cancellationToken);
+
         transaction.UserId = user.Id;
 
         var transactionSignature = _encryptionService.HashWithSalt(transaction);
