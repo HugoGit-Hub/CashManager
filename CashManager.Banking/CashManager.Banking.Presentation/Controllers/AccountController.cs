8981fc1d735a79bf6e23fa31cb06eda20d92abab
crysy-rThomas <ryan.thomas39500@gmail.com>
2023-11-14 12:12:13 +0100

    Get Account Controller

---
+++ CashManager.Banking/CashManager.Banking.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using CashManager.Banking.Domain.Accounts;
+using CashManager.Banking.Presentation.Dto;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashManager.Banking.Presentation.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly IAccountService _accountService;
+
+    public AccountController(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    [HttpGet(nameof(Get))]
+    public async Task<AccountDto> Get(CancellationToken cancellationToken)
+    {
+        var account = await _accountService.Get(cancellationToken);
+
+        return account.Adapt<AccountDto>();
+    }
+}
+
+
