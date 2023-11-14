ec07a4299153d3eeb1b2c9f522c0c22dc3cf5e3d
Hugo <hugo.decuq.34@icloud.com>
2023-11-14 08:18:04 +0100

    Migration

--- CashManager.Banking/CashManager.Banking.Infrastructure/Migrations/20231113072202_TransactionAmount.Designer.cs
+++ CashManager.Banking/CashManager.Banking.Infrastructure/Migrations/20231113072202_TransactionAmount.Designer.cs
@@ -4,6 +4,7 @@ using CashManager.Banking.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CashManager.Banking.Infrastructure.Migrations
 {
     [DbContext(typeof(CashManagerBankingContext))]
-    partial class CashManagerBankingContextModelSnapshot : ModelSnapshot
+    [Migration("20231113072202_TransactionAmount")]
+    partial class TransactionAmount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CashManager.Banking/CashManager.Banking.Infrastructure/Migrations/20231113072202_TransactionAmount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashManager.Banking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransactionAmount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Amount",
+                table: "Transactions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Transactions");
+        }
+    }
+}
