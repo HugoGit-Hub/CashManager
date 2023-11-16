618e840dc46522e95f497a4113f9b7bc2fee669c
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-11-16 21:02:01 +0100

    Migration

---
+++ CashManager.Banking/CashManager.Banking.Infrastructure/Migrations/20231116200034_TransactionGuid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashManager.Banking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransactionGuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Guid",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "Transactions");
+        }
+    }
+}
