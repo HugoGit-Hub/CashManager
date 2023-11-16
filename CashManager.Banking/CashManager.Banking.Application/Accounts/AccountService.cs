9fe2ef27127bc1e3aa4fcba9ccb486e63085bec1
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-11-16 20:46:12 +0100

    Account

--- CashManager.Banking/CashManager.Banking.Application/Accounts/AccountService.cs
+++ CashManager.Banking/CashManager.Banking.Application/Accounts/AccountService.cs
@@ -13,14 +13,14 @@ internal class AccountService : IAccountService
 
     public async Task Transaction(string creditor, string debtor, double amount, CancellationToken cancellationToken)
     {
+        var creditorAccount = await _accountRepository.Get(creditor, cancellationToken) ?? throw new NullAccountException();
+        var debtorAccount = await _accountRepository.Get(debtor, cancellationToken) ?? throw new NullAccountException();
         var result = await CanAccountBeDebited(creditor, amount, cancellationToken);
         if (!result)
         {
             throw new NonDebatableAccountException();
         }
 
-        var creditorAccount = await _accountRepository.Get(creditor, cancellationToken);
-        var debtorAccount = await _accountRepository.Get(debtor, cancellationToken);
         creditorAccount.Value -= amount;
         debtorAccount.Value += amount;
         await _accountRepository.Update(creditorAccount, cancellationToken);
@@ -29,7 +29,7 @@ internal class AccountService : IAccountService
 
     private async Task<bool> CanAccountBeDebited(string number, double amount, CancellationToken cancellationToken)
     {
-        var result = await _accountRepository.Get(number, cancellationToken);
+        var result = await _accountRepository.Get(number, cancellationToken) ?? throw new NullAccountException();
         
         return !(result.Value < amount);
     }
---
+++ CashManager.Banking/CashManager.Banking.Application/Accounts/NullAccountException.cs
@@ -0,0 +1,18 @@
+﻿namespace CashManager.Banking.Application.Accounts;
+
+public class NullAccountException : Exception
+{
+    public NullAccountException()
+    {
+    }
+
+    public NullAccountException(string message)
+        : base(message)
+    {
+    }
+
+    public NullAccountException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
