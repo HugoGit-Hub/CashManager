78120ec34a296db3a5d6c4b40ddf14ff0dc5df16
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-11-20 07:19:34 +0100

    Navigation bar

--- cashmanager-banking-web/app/components/NavigationBar.tsx
+++ cashmanager-banking-web/app/components/NavigationBar.tsx
@@ -2,29 +2,37 @@ import Link from "next/link"
 
 function NavigationBar() {
     return (
-        <div className="drawer">
-            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
-            <div className="drawer-content flex flex-col">
-                <div className="w-full navbar bg-base-300">
-                    <div className="flex-none lg:hidden">
-                        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                        </label>
-                    </div> 
-                <div className="flex-1 px-2 mx-2">Nom de la banque</div>
-                    <div className="flex-none hidden lg:block">
-                        <button className="btn btn-primary mx-2">Mes comptes</button>
-                        <Link className="btn btn-error mx-2" href={"/"}>Déconnexion</Link>
-                    </div>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
+                        </svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <Link className="m-2" href={"/account"}>Mes comptes</Link>
+                        <Link className="text-error m-2" href={"/"}>Déconnexion</Link>
+                    </ul>
                 </div>
-                Content
-            </div> 
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
-                <ul className="menu p-4 w-80 min-h-full bg-base-200">
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
+            </div>
+            <div className="navbar-center">
+                <a className="btn btn-ghost text-xl">Nom de la banque</a>
+            </div>
+            <div className="navbar-end">
+                <button className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
+                <button className="btn btn-ghost btn-circle">
+                    <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                        </svg>
+                        <span className="badge badge-xs badge-primary indicator-item"></span>
+                    </div>
+                </button>
             </div>
         </div>
     )
