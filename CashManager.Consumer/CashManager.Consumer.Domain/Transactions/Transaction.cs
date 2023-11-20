0c12c168fab82e7330038bf4f78ba0267ef3c703
crysy-rThomas <ryan.thomas39500@gmail.com>
2023-11-20 14:19:51 +0100

    Domain init

---
+++ CashManager.Consumer/CashManager.Consumer.Domain/Transactions/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using CashManager.Consumer.Domain.Transaction;
+using System.ComponentModel.DataAnnotations;
+
+namespace CashManager.Consumer.Domain.Transactions;
+
+public class Transaction
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(34)]
+    public string Creditor { get; set; } = null!;
+
+    [Required]
+    [MaxLength(34)]
+    public string Debtor { get; set; } = null!;
+
+    [Required]
+    public TransactionTypeEnum Type { get; set; }
+
+    [Required]
+    public double Amount { get; set; }
+
+    [Required]
+    public TransactionStateEnum State { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+
+    [Required]
+    public Guid Guid { get; set; }
+
+}
