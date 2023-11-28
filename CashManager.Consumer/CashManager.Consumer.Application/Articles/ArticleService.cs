f3709638092d7a2f2e5494a3cd493844292b0d53
crysy-rThomas <ryan.thomas39500@gmail.com>
2023-11-28 12:09:56 +0100

    Application

---
+++ CashManager.Consumer/CashManager.Consumer.Application/Articles/ArticleService.cs
@@ -0,0 +1,26 @@
+﻿using CashManager.Consumer.Domain.Articles;
+using CashManager.Consumer.Infrastructure.Articles;
+
+
+namespace CashManager.Consumer.Application.Articles;
+
+public class ArticleService : IArticleService
+{
+
+    private readonly IArticleRepository _articleRepository;
+
+    public ArticleService(IArticleRepository articleRepository)
+    {
+        _articleRepository = articleRepository;
+    }
+
+    public Task<Article?> Get(int id, CancellationToken cancellationToken)
+    {
+        return _articleRepository.Get(id, cancellationToken);
+    }
+
+    public Task<IEnumerable<Article>> GetAll(CancellationToken cancellationToken)
+    {
+        return _articleRepository.GetAll(cancellationToken);
+    }
+}
---
+++ CashManager.Consumer/CashManager.Consumer.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using CashManager.Consumer.Application.Articles;
+using CashManager.Consumer.Domain.Articles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashManager.Consumer.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IArticleService, ArticleService>();
+        
+        return services;
+    }
+}
