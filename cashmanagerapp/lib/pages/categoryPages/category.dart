3d5e96fb7d5e94f5f5da4c51eccfc62b986b2672
crysy-rThomas <ryan.thomas39500@gmail.com>
2023-11-30 16:05:55 +0100

    category pages

---
+++ cashmanagerapp/lib/pages/categoryPages/category.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Category extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Category'),
+      ),
+      body: Center(
+        child: Text('This is Category home page'),
+      ),
+    );
+  }
+}
