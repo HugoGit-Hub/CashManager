8716542e6d616b03d917d6ad7ebca50bc0d46740
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-12-03 09:51:49 +0100

    Domain

---
+++ CashManager.Consumer/CashManager.Consumer.Domain/Transactions/ITransactionService.cs
@@ -0,0 +1,10 @@
+﻿namespace CashManager.Consumer.Domain.Transactions;
+
+public interface ITransactionService
+{
+    public Task<Transaction> Post(Transaction transaction,CancellationToken cancellationToken);
+
+    public Task<Transaction> Put(Transaction transaction,CancellationToken cancellationToken); 
+
+    public Task<Transaction> Get(Guid guid,CancellationToken cancellationToken);
+}
