f65349d12c8fad86df3d85ec6eaebc4fc9781cda
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-12-03 09:52:07 +0100

    Infrastructure

--- CashManager.Consumer/CashManager.Consumer.Infrastructure/DependencyInjection.cs
+++ CashManager.Consumer/CashManager.Consumer.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,9 @@
 ﻿using CashManager.Consumer.Domain.Articles;
+using CashManager.Consumer.Domain.HttpClients;
+using CashManager.Consumer.Domain.Transactions;
 using CashManager.Consumer.Infrastructure.Articles;
+using CashManager.Consumer.Infrastructure.HttpClients;
+using CashManager.Consumer.Infrastructure.Transactions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashManager.Consumer.Infrastructure;
@@ -9,6 +13,8 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddScoped<IArticleRepository, ArticleRepository>();
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
+        services.AddScoped<IHttpClientService, HttpClientService>();
 
         return services;
     }
---
+++ CashManager.Consumer/CashManager.Consumer.Infrastructure/HttpClients/HttpClientService.cs
@@ -0,0 +1,26 @@
+﻿using CashManager.Consumer.Domain.HttpClients;
+using CashManager.Consumer.Domain.Transactions;
+using System.Text;
+using System.Text.Json;
+
+namespace CashManager.Consumer.Infrastructure.HttpClients;
+
+internal class HttpClientService : IHttpClientService
+{
+    public async Task Post(Transaction transaction, CancellationToken cancellationToken)
+    {
+        var uri = new Uri("https://localhost:7154/api/Transaction/Post");
+        var client = new HttpClient
+        {
+            BaseAddress = new Uri(uri.GetLeftPart(UriPartial.Authority))
+        };
+        client.DefaultRequestHeaders.Add("ApiKey", ",.PjqV#.|X>kgp{?JsygExquC;tVuf5%");
+
+        var content = new StringContent(JsonSerializer.Serialize(transaction), Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(uri, content, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpCallbackRequestException($"Something went wrong during the http callback request : {response.ReasonPhrase}");
+        }
+    }
+}
