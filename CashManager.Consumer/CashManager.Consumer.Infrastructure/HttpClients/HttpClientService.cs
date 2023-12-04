e5cf162322d74f2d96c5458724c79a997f666323
Hugo <hugo.decuq.34@icloud.com>
2023-12-04 13:51:30 +0100

    Infrastructure

--- CashManager.Consumer/CashManager.Consumer.Infrastructure/HttpClients/HttpClientService.cs
+++ CashManager.Consumer/CashManager.Consumer.Infrastructure/HttpClients/HttpClientService.cs
@@ -1,4 +1,5 @@
-﻿using CashManager.Consumer.Domain.HttpClients;
+﻿using CashManager.Consumer.Domain.ErrorHandling;
+using CashManager.Consumer.Domain.HttpClients;
 using CashManager.Consumer.Domain.Transactions;
 using System.Text;
 using System.Text.Json;
@@ -7,7 +8,7 @@ namespace CashManager.Consumer.Infrastructure.HttpClients;
 
 internal class HttpClientService : IHttpClientService
 {
-    public async Task Post(Transaction transaction, CancellationToken cancellationToken)
+    public async Task<Result> Post(Transaction transaction, CancellationToken cancellationToken)
     {
         var uri = new Uri("https://localhost:7154/api/Transaction/Post");
         var client = new HttpClient
@@ -18,9 +19,9 @@ internal class HttpClientService : IHttpClientService
 
         var content = new StringContent(JsonSerializer.Serialize(transaction), Encoding.UTF8, "application/json");
         var response = await client.PostAsync(uri, content, cancellationToken);
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new HttpCallbackRequestException($"Something went wrong during the http callback request : {response.ReasonPhrase}");
-        }
+        
+        return !response.IsSuccessStatusCode 
+            ? Result.Failure(HttpClientErrors.HttpCallbackRequestError) 
+            : Result.Success();
     }
 }
