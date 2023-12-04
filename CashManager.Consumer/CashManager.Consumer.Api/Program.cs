0e0eeb6c881793197c346d0f5ff1c0766aaac608
Hugo <hugo.decuq.34@icloud.com>
2023-12-04 16:44:10 +0100

    Disocate presentation and api

--- CashManager.Consumer/CashManager.Consumer.Api/Program.cs
+++ CashManager.Consumer/CashManager.Consumer.Api/Program.cs
@@ -1,26 +1,19 @@
 using CashManager.Consumer.Application;
 using CashManager.Consumer.Infrastructure;
-using CashManager.Consumer.Infrastructure.Context;
-using Microsoft.EntityFrameworkCore;
+using CashManager.Consumer.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<CashManagerConsumerContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddApplication()
-                .AddInfrastructure();
+                .AddInfrastructure(builder.Configuration)
+                .AddPresentation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
