bf97571c5257fb8c553a79ea74691d70a2811dd3
Hugo <hugo.decuq.34@icloud.com>
2023-12-05 08:24:36 +0100

    Presentation DI

---
+++ CashManager.Banking/CashManager.Banking.Presentation/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CashManager.Banking.Presentation;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        return services;
+    }
+}
