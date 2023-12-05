c5ae208fc978c3541fc29975c745e0babee8852d
Hugo <hugo.decuq.34@icloud.com>
2023-12-05 08:22:49 +0100

    New api project

---
+++ CashManager.Banking/CashManager.Banking.Api/Configuration/Schemes/ApiKeyOption.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace CashManager.Banking.Api.Configuration.Schemes;
+
+public class ApiKeyOption : AuthenticationSchemeOptions
+{
+    public string HeaderField { get; set; } = null!;
+
+    public string HeaderAttemptedValue { get; set; } = null!;
+}
