6490a62f6a9d319347b79eccea2b632784d8a14a
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-12-10 19:56:30 +0100

    Resolve duplication of code

--- CashManager.Banking/CashManager.Banking.Application/Accounts/AccountResponse.cs
+++ CashManager.Banking/CashManager.Banking.Application/Accounts/AccountResponse.cs
@@ -1,26 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CashManager.Banking.Domain.Accounts;
 
 namespace CashManager.Banking.Application.Accounts;
 
-public record AccountResponse
+public record AccountResponse : AccoutBase
 {
     public int Id { get; set; }
-
-    [Required]
-    [MaxLength(50)]
-    public string Owner { get; set; } = null!;
-
-    [Required]
-    [MaxLength(34)]
-    public string Number { get; set; } = null!;
-
-    [Required]
-    public double Value { get; set; }
-
-    [Required]
-    [DataType(DataType.Date)]
-    public DateTime OpenDateTime { get; set; }
-
-    [DataType(DataType.Date)]
-    public DateTime CloseDateTime { get; set; }
 }
---
+++ CashManager.Banking/CashManager.Banking.Domain/Accounts/AccoutBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CashManager.Banking.Domain.Accounts;
+
+public record AccoutBase
+{
+    [Required]
+    [MaxLength(50)]
+    public string Owner { get; set; } = null!;
+
+    [Required]
+    [MaxLength(34)]
+    public string Number { get; set; } = null!;
+
+    [Required]
+    public double Value { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime OpenDateTime { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime CloseDateTime { get; set; }
+}
