b8f5a815e5f4564794cc7bf27dafd69a063ed313
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-12-14 09:14:45 +0100

    Update + Refacto AuthenticationController

--- CashManager.Consumer/CashManager.Consumer.Api/Controllers/AuthenticationController.cs
+++ CashManager.Consumer/CashManager.Consumer.Api/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
-﻿using CashManager.Consumer.Application.Authentication.Register;
+﻿using CashManager.Consumer.Application.Authentication.Login;
+using CashManager.Consumer.Application.Authentication.Register;
+using CashManager.Consumer.Domain.Authentication;
 using CashManager.Consumer.Domain.ErrorHandling;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -16,15 +18,32 @@ public class AuthenticationController : Controller
         _sender = sender;
     }
 
-    [HttpPost]
-    public ActionResult<Result<string>> Register(RegisterRequest registerRequest)
+    [HttpPost(nameof(Register))]
+    public async Task<ActionResult<Result<string>>> Register(RegisterRequest registerRequest)
     {
-        var handler = _sender.Send(new RegisterCommand(registerRequest));
-        if (handler.Result.IsFailure)
+        var handler = await _sender.Send(new RegisterCommand(registerRequest));
+        if (handler.IsFailure)
         {
-            return BadRequest(handler.Result.Error);
+            return BadRequest(handler.Error);
         }
 
-        return Ok(handler.Result.Value);
+        return Ok(handler.Value);
+    }
+
+    [HttpPost(nameof(Login))]
+    public async Task<ActionResult<Result<string>>> Login(LoginRequest loginRequest)
+    {
+        var handler = await _sender.Send(new LoginQuery(loginRequest));
+        if (handler.Error == AuthenticationErrors.WrongCredentialsError)
+        {
+            return Unauthorized(handler.Error);
+        }
+
+        if (handler.IsFailure)
+        {
+            return BadRequest(handler.Error);
+        }
+
+        return Ok(handler.Value);
     }
 }
