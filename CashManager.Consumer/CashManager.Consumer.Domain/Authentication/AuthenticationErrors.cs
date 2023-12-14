1c639f75e5cd52fc74e849820fea43a59c6ebb97
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-12-14 09:16:31 +0100

    Create Errors

---
+++ CashManager.Consumer/CashManager.Consumer.Domain/Authentication/AuthenticationErrors.cs
@@ -0,0 +1,9 @@
+﻿using CashManager.Consumer.Domain.ErrorHandling;
+
+namespace CashManager.Consumer.Domain.Authentication;
+
+public static class AuthenticationErrors
+{
+    public static readonly Error WrongCredentialsError = new(
+        "Authentication.WrongCredentials", "Provided email and password doesn't match");
+}
