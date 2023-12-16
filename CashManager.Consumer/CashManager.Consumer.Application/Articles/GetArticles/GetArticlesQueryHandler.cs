8b29fc88143d3415769bc5a5bfab338fe4550a47
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-12-16 17:33:15 +0100

    Queries and Handlers

---
+++ CashManager.Consumer/CashManager.Consumer.Application/Articles/GetArticles/GetArticlesQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using CashManager.Consumer.Domain.Articles;
+using CashManager.Consumer.Domain.ErrorHandling;
+using MediatR;
+
+namespace CashManager.Consumer.Application.Articles.GetArticles;
+
+internal class GetArticlesQueryHandler : IRequestHandler<GetArticlesQuery, Result<IEnumerable<Article>>>
+{
+    private readonly IArticleService _articleService;
+
+    public GetArticlesQueryHandler(IArticleService articleService)
+    {
+        _articleService = articleService;
+    }
+
+    public async Task<Result<IEnumerable<Article>>> Handle(GetArticlesQuery request, CancellationToken cancellationToken)
+    {
+        var articles = await _articleService.GetAll(cancellationToken);
+
+        return Result<IEnumerable<Article>>.Success(articles);
+    }
+}
