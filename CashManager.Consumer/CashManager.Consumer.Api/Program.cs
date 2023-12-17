4d744e5752bc167e4811c38a6741371e45f253cb
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-12-17 10:35:54 +0100

    Middleware CORS + Authorization + Authentication (Swagger security service)

--- CashManager.Consumer/CashManager.Consumer.Api/Program.cs
+++ CashManager.Consumer/CashManager.Consumer.Api/Program.cs
@@ -3,6 +3,11 @@ using CashManager.Consumer.Application;
 using CashManager.Consumer.Infrastructure;
 using CashManager.Consumer.Presentation;
 using Mapster;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +16,47 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblies(AssemblyMarker.Assembly));
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+        });
+});
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Authorization using Bearer scheme",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            RequireExpirationTime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("lijebfoibhviezrbnviezrhbvierbvpizerubvfsipudfdvnpi")), //TODO: Replace this hard coded token key
+            ValidIssuer = "CashManager.Consumer.Client", //TODO: Set this in config file
+            ValidAudience = "CashManager" //TODO: Set this in config file
+        };
+    });
 
 builder.Services.AddApplication()
                 .AddInfrastructure(builder.Configuration)
@@ -26,7 +72,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
