3320f23fcc0395b4edaf490e3931dbf4afbed5fa
Hugo <hugo.decuq.34@icloud.com>
2023-12-18 14:59:28 +0100

    Infrastructure

---
+++ CashManager.Consumer/CashManager.Consumer.Infrastructure/CartItems/CartItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using CashManager.Consumer.Application.CartItems;
+using CashManager.Consumer.Domain.CartItems;
+using CashManager.Consumer.Domain.ErrorHandling;
+using CashManager.Consumer.Infrastructure.Context;
+
+namespace CashManager.Consumer.Infrastructure.CartItems;
+
+internal class CartItemRepository : ICartItemRepository
+{
+    private readonly CashManagerConsumerContext _context;
+
+    public CartItemRepository(CashManagerConsumerContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CartItem> Create(CartItem cartItem, CancellationToken cancellationToken)
+    {
+        var createCartItem = await _context.CartItems.AddAsync(cartItem, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return createCartItem.Entity;
+    }
+}
