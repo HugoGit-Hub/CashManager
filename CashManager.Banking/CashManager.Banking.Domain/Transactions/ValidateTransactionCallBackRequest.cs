a8f2d1a3e0659b9f281665fd1fd25089d523cc68
Hugo <hugo.decuq.34@icloud.com>
2023-12-19 22:48:48 +0100

    Banking Infrastructure

---
+++ CashManager.Banking/CashManager.Banking.Domain/Transactions/ValidateTransactionCallBackRequest.cs
@@ -0,0 +1,38 @@
+﻿using CashManager.Banking.Domain.Transactions;
+using System.ComponentModel.DataAnnotations;
+
+namespace CashManager.Banking.Application.Transactions;
+
+public record ValidateTransactionCallBackRequest
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(34)]
+    public string Creditor { get; set; } = null!;
+
+    [Required]
+    [MaxLength(34)]
+    public string Debtor { get; set; } = null!;
+
+    [Required]
+    public TransactionTypeEnum Type { get; set; }
+
+    [Required]
+    public double Amount { get; set; }
+
+    [Required]
+    public TransactionStateEnum State { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+
+    [Required]
+    public Guid Guid { get; set; }
+
+    public string? Signature { get; set; }
+
+    [Required]
+    public string Url { get; set; } = null!;
+}
