47046e040135ccbfe5891e688d1ec5b3def58f55
Inès Garcia <ines.garcia@epitech.eu>
2023-12-23 18:02:06 +0100

    CartPage

--- cashmanagerapp/lib/pages/cartPages/cart.dart
+++ cashmanagerapp/lib/pages/cartPages/cart.dart
@@ -1,14 +1,171 @@
+import 'package:cashmanagerapp/models/cartitemmodel.dart';
+import 'package:cashmanagerapp/services/articleservice.dart';
+import 'package:cashmanagerapp/widgets/button_place_order.dart';
+import 'package:cashmanagerapp/widgets/quantity_selector.dart';
 import 'package:flutter/material.dart';
 
-class Cart extends StatelessWidget {
+class Cart extends StatefulWidget {
+  Cart({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  List<CartItemModel> cartitems = [];
+  @override
+  void initState() {
+    super.initState();
+    getAllCartItem();
+  }
+
+  getAllCartItem() async {
+    final List<CartItemModel> cartitems =
+        (await ArticleService().getAllCartItem()).toList();
+    setState(() {
+      this.cartitems = cartitems;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart'),
       ),
-      body: Center(
-        child: Text('This is Cart'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Item details'.toUpperCase(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cartitems.length,
+              itemBuilder: (context, index) {
+                var cartitem = cartitems[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          width: 50.0,
+                          height: 70.0,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(cartitem.imageUrl),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                cartitem.articleName,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                              Text(
+                                cartitem.totalArticlePrice.toString(),
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          QuantitySelector(
+                            price: 0,
+                            onQuantityChanged: (quantity, total) {},
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.delete),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Divider(
+            thickness: 3.0,
+            height: 1.0,
+            color: Colors.grey[400],
+          ),
+          SizedBox(height: 10.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Text(
+              'Add items worth 5€ more for FREE delivery',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14.0,
+                color: Colors.green,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 16.0,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Total',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    Text(
+                      'Price details',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
+                ),
+                ButtonPlaceOrder(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ cashmanagerapp/lib/widgets/button_place_order.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ButtonPlaceOrder extends StatelessWidget {
+  const ButtonPlaceOrder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed(
+              '/'); // Ajoutez la redirection vers la page de paiements
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.yellow[600]!,
+          foregroundColor: Colors.white,
+          padding: EdgeInsets.only(
+            top: 15.0,
+            bottom: 15.0,
+            left: 40.0,
+            right: 40.0,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+            side: BorderSide(
+              color: Colors.yellow[600]!,
+              width: 1.0,
+            ),
+          ),
+          elevation: 4,
+        ),
+        child: Column(
+          children: [
+            Text(
+              'Place order'.toUpperCase(),
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
