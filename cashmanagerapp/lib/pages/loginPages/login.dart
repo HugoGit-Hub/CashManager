3dfe534b08b7a98966c5b9ff2569bf7668f03c88
crysy-rThomas <ryan.thomas39500@gmail.com>
2024-01-07 15:58:57 +0100

    set prob usage

--- cashmanagerapp/lib/pages/loginPages/login.dart
+++ cashmanagerapp/lib/pages/loginPages/login.dart
@@ -87,15 +87,12 @@ class _LoginState extends State<Login> {
                               r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
                           .hasMatch(emailController.text);
                       if (emailValid) {
-                        // try {
-                        // // await AuhtenticationService().login(emailController.text, passwordController.text);
-                        // } catch (e) {
-                        //   ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                        // }
+                        try {
+                          await AuhtenticationService().login(emailController.text, passwordController.text);
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
                       }
-                      // FOR DEVELOPMENT PURPOSES ONLY
-                      await AuhtenticationService()
-                          .login("user@example.com", "string");
                       Navigator.pushNamed(context, '/');
                     },
                     style: ElevatedButton.styleFrom(
